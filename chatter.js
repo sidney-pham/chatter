e987771cdbc3e5b2ff067e928032233e8be355b8
Sidney Pham <sidneypham@gmail.com>
2016-06-05 04:43:12 +1000

    added friends functionality. users can now add and remove friends

--- chatter.js
+++ chatter.js
@@ -1,3 +1,5 @@
+Chats = new Mongo.Collection("chats");
+
 Router.configure({
     layoutTemplate: "main"
 });
@@ -91,7 +93,8 @@ if (Meteor.isClient) {
                 var userProfile = {
                     firstName: firstName,
                     lastName: lastName,
-                    fullName: firstName + " " + lastName
+                    fullName: firstName + " " + lastName,
+                    friends: []
                 }
 
                 Accounts.createUser({
@@ -157,8 +160,102 @@ if (Meteor.isClient) {
             Session.set("selectedButton", "none");
         }
     });
+
+    Template.addFriends.helpers({
+        "friendsToAdd": function() {
+            var friendsToAdd = [];
+
+            var allUsers = Meteor.users.find().fetch();
+            var currentUser = Meteor.userId();
+            var friendsList = Meteor.users.findOne({_id: currentUser}).profile.friends;
+
+            allUsers.forEach(function(user) {
+                var otherPlayerId = user._id;
+                var isFriend = friendsList.indexOf(otherPlayerId) != -1;
+
+                if (!isFriend && otherPlayerId != currentUser) {
+                    friendsToAdd.push(user);
+                }
+            });
+
+            return friendsToAdd;
+        }
+    });
+
+    // When you add a friend, you have to update your new friend's
+    // friend list so that you are their friend as well. This requires
+    // more privileges, so you need to run this from a method.
+    Template.addFriends.events({
+        "change .addFriendCheckbox": function() {
+            var currentUser = Meteor.userId();
+            var newFriendId = this._id;
+
+            Meteor.call("addFriend", currentUser, newFriendId);
+        }
+    });
+
+    Template.chatWindow.helpers({
+        // We can't just return friendsList since friendsList contains
+        // the IDs of each friend and not the entire user object.
+        "friends": function() {
+            var friends = []
+            var allUsers = Meteor.users.find().fetch();
+            var currentUser = Meteor.userId();
+            var friendsList = Meteor.users.findOne({_id: currentUser}).profile.friends;
+
+            allUsers.forEach(function(user) {
+                var otherPlayerId = user._id;
+                var isFriend = friendsList.indexOf(otherPlayerId) != -1;
+
+                if (isFriend) {
+                    friends.push(user);
+                }
+            });
+
+            return friends;
+        }
+    });
+
+    Template.chatWindow.events({
+        "click .removeFriend": function(event) {
+            event.preventDefault();
+            var currentUser = Meteor.userId();
+            var friendId = this._id;
+
+            Meteor.call("removeFriend", currentUser, friendId);
+        }
+    });
 }
 
 if (Meteor.isServer) {
     console.log("CHATTER IS RUNNING!");
 }
+
+Meteor.methods({
+    "addFriend": function(currentUser, newFriendId) {
+        var userProfile = Meteor.users.findOne({_id: currentUser}).profile;
+        userProfile.friends.push(newFriendId);
+
+        Meteor.users.update({_id: currentUser}, {$set: {profile: userProfile}});
+
+        var friendProfile = Meteor.users.findOne({_id: newFriendId}).profile;
+        friendProfile.friends.push(currentUser);
+
+        Meteor.users.update({_id: newFriendId}, {$set: {profile: friendProfile}});
+    }, "removeFriend": function(currentUser, friendId) {
+        var userProfile = Meteor.users.findOne({_id: currentUser}).profile;
+        // Removes friend from userProfile.friends
+        var indexToRemove = userProfile.friends.indexOf(friendId);
+        userProfile.friends.splice(indexToRemove, 1); 
+
+        Meteor.users.update({_id: currentUser}, {$set: {profile: userProfile}});
+
+
+        var friendProfile = Meteor.users.findOne({_id: friendId}).profile;
+        // Removes currentUser from friendProfile.friends
+        indexToRemove = friendProfile.friends.indexOf(currentUser);
+        friendProfile.friends.splice(indexToRemove, 1); 
+
+        Meteor.users.update({_id: friendId}, {$set: {profile: friendProfile}});
+    }
+});
